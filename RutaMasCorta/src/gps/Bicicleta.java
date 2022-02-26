301e6e24ae2f87def074161876e2d456df6f7913
carpe <carpe@LAPTOP-HAFDREU4>
2022-02-26 19:00:16 +0100

    Camino más corto corregido

--- RutaMasCorta/src/gps/Bicicleta.java
+++ RutaMasCorta/src/gps/Bicicleta.java
@@ -13,7 +13,7 @@ public class Bicicleta {
         String marca = "Orbea";
     }
     
-    public static String rutaCorta(int [] camino1, int [] camino2, int [] camino3)
+   /* public static String rutaCorta(int [] camino1, int [] camino2, int [] camino3)
     {
         int sum1 = 0;
         int sum2 = 0;
@@ -49,14 +49,66 @@ public class Bicicleta {
             }
         }
         
+    }*/
+    
+    public static String verdaderRutaCorta(int [] camino1, int [] camino2, int [] camino3)
+    {
+        int sum1 = 0;
+        int sum2 = 0;
+        int sum3 = 0;
+        
+        for(int i=0; i<camino1.length; i++) {
+            
+            sum1 += camino1[i]; 
+            }
+        System.out.println("El recorrido del camino 1 tiene " +sum1 + " Km");
+        
+        for(int i=0; i<camino2.length; i++) {
+            
+            sum2 += camino2[i]; 
+            }
+        System.out.println("El recorrido del camino 2 tiene " +sum2 + " Km");
+        
+        for(int i=0; i<camino3.length; i++) {
+            
+            sum3 += camino3[i]; 
+            }
+        System.out.println("El recorrido del camino 2 tiene " + sum3 + " Km");
+        
+        if(sum1 < sum2 && sum1 < sum3)
+        {
+            return "camino 1";
+            
+        }else if(sum2 < sum3 && sum2 < sum1)
+        {
+            return "camino 2";
+            
+        }else if(sum3 < sum1 && sum3 < sum2)
+        {
+            return "camino 3";
+            
+        }else{
+            if(sum1 == sum2 && sum2 == sum3)
+            {
+                return "camino 1, 2 y 3";
+            }else if(sum1 == sum3)
+            {
+                return "caminos 1 y 3";
+            }else if(sum2 == sum3){
+                return "caminos 2 y 3";
+            }else{
+                return "caminos 1 y 2";
+            }
+        }
     }
 
     public static void main(String[] args) {
         Bicicleta bici = new Bicicleta();
         int[] camino1 = {12,34,21,46,25};
-        int[] camino2 = {24,1,5,64,10,15,21};
+        int[] camino2 = {24,1,5,64,10,15,21};//24,1,5,64,10,15,21
         int[] camino3 = {1,5,6,7,8,10,20,46,104};
-        System.out.println("El camino mÃ¡s corto es:"+Bicicleta.rutaCorta(camino1,camino2,camino3)+".");
+        //System.out.println("El camino más corto es:"+Bicicleta.rutaCorta(camino1,camino2,camino3)+".");
+        System.out.println("El camino verdaderamente más corto es el: "+Bicicleta.verdaderRutaCorta(camino1,camino2,camino3)+".");
     }
 }
 
